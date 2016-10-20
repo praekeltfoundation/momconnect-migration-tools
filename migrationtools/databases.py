e3a01d63e967a44f9f6e0e8d775dd74713ddb535
Jason Paidoussi <jason@paidoussi.net>
2016-10-20 16:10:09 +0200

    Added  existing registration lookups

--- migrationtools/databases.py
+++ migrationtools/databases.py
@@ -1,4 +1,6 @@
 from sqlalchemy import create_engine, MetaData
+from sqlalchemy.sql import select
+from sqlalchemy.sql.expression import null
 
 
 class Database(object):
@@ -26,9 +28,37 @@ class NDOHControl(Database):
     def _setup_tables(self, meta):
         self.subscription = meta.tables['subscription_subscription']
         self.registration = meta.tables['registration_registration']
+        self.source = meta.tables['registration_source']
         self.messageset = meta.tables['subscription_messageset']
         self.crontabschedule = meta.tables['djcelery_crontabschedule']
         self.periodtask = meta.tables['djcelery_periodictask']
+
+    def get_registrations(self, start_id=None, end_id=None, limit=None):
+        """Get all the current registration records order by pk (id) in
+        ascending order.
+
+        start_id and end_id can be given as inclusive boundries of which rows
+        to select.
+
+        limit can be given has a hard cutoff on the number of rows to select.
+        """
+        statement = select([self.registration, self.source])\
+            .select_from(self.registration.join(self.source))
+        where = None
+        if start_id is not None:
+            where = (self.registration.c.id >= start_id)
+        if end_id is not None:
+            if where is not None:
+                where = where & (self.registration.c.id <= end_id)
+            else:
+                where = (self.registration.c.id <= end_id)
+        if where is not None:
+            statement = statement.where(where)
+
+        statement = statement.order_by(self.registration.c.id)
+        if limit is not None:
+            statement = statement.limit(limit)
+        return self.connection.execute(statement)
 
 
 class NDOHHub(Database):
@@ -42,6 +72,38 @@ class SeedIdentity(Database):
 
     def _setup_tables(self, meta):
         self.identity = meta.tables['identities_identity']
+
+    def lookup_identity_with_msdisdn(self, msisdn):
+        statement = select([self.identity])\
+            .where(self.identity.c.details[('addresses', 'msisdn', msisdn)]
+                   != null())
+        return self.connection.execute(statement).fetchone()
+
+    def create_identity_details(self, msisdn, lang_code, consent, sa_id_no, mom_dob, source, last_mc_reg_on):
+        return {
+            'default_addr_type': 'msisdn',
+            'addresses': {
+                'msisdn': {
+                    msisdn: {'default': True}
+                }
+            },
+            'lang_code': lang_code,
+            'consent': consent,
+            'sa_id_no': sa_id_no,
+            'mom_dob': mom_dob,
+            'source': source,
+            'last_mc_reg_on': last_mc_reg_on
+        }
+
+    def create_identity(self, details, operator, created_at, updated_at, created_by, updated_by):
+        # version
+        # communicate_through
+        # operator
+        # created_at
+        # updated_at
+        # created_by
+        # updated_by
+        pass
 
 
 class SeedSBM(Database):
--- migrationtools/migrations.py
+++ migrationtools/migrations.py
@@ -1,3 +1,6 @@
+import click
+from profilehooks import profile
+
 from . import databases
 
 
@@ -24,7 +27,31 @@ class Migrator(object):
         except AttributeError:
             raise ImproperlyConfigured("Invalid or missing database configs.")
 
+    @profile
+    def migrate_registrations(self):
+        for registration in self.ndoh_control.get_registrations():
+            # Check for existing Seed Identity for mom_msisdn:
+            mom_msisdn = registration[self.ndoh_control.registration.c.mom_msisdn]
+            identity = self.seed_identity.lookup_identity_with_msdisdn(mom_msisdn)
+            if identity is not None:
+                # No existing Identity, so create it.
+                lang = registration[self.ndoh_control.registration.c.mom_lang]
+                source = registration[self.ndoh_control.registration.c.source_id]
+                details = self.seed_identity.create_identity_details(
+                    mom_msisdn, lang, registration[self.ndoh_control.registration.c.consent],
+                    registration[self.ndoh_control.registration.c.mom_id_no],
+                    registration[self.ndoh_control.registration.c.mom_dob],
+                    source, source)
+                identity = self.seed_identity.create_identity(
+                    details, None, registration[self.ndoh_control.registration.c.created_at],
+                    registration[self.ndoh_control.registration.c.updated_at], None, None)
+
+                click.echo(identity)
+
+            # Create ndoh-hub Registration.
+
     def full_migration(self):
         # Migrate registrations
+        self.migrate_registrations()
         # Migrate subscriptions
-        pass
+        # Migrate nurseconnect
