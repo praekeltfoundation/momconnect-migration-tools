bc986d87f20b96bb4ee4abacbbe2164911ecf022
Jason Paidoussi <jason@paidoussi.net>
2016-10-25 20:44:32 +0200

    Added first actual test

--- tests/test_migrations.py
+++ tests/test_migrations.py
@@ -1,12 +1,19 @@
 import os.path
+from datetime import datetime
 import subprocess32 as subprocess
 
-import click
 import pytest
 from sqlalchemy.engine.url import make_url
+from sqlalchemy.sql import select
+from sqlalchemy.sql.expression import null
 from sqlalchemy_utils import database_exists, create_database, drop_database
 
 from migrationtools import migrations
+
+
+def mock_echo(message, nl=False, err=False):
+    # Do nothing
+    pass
 
 
 def import_sql_schema(path, db_name):
@@ -37,15 +44,68 @@ def migrator(config, request):
             create_database(db_url)
             url = make_url(db_url)
             import_sql_schema(request.fspath.dirname, url.database)
-    yield migrations.Migrator(config, echo=click.echo)
+    yield migrations.Migrator(config, echo=mock_echo)
     # Drop the test DBs on teardown.
     for db_url in config.DATABASES.values():
         if database_exists(db_url):
             drop_database(db_url)
 
 
-def test_migrator(migrator):
-    assert 1 == 0
-# populat test ndoh-control DB with test data
+@pytest.fixture(scope='module')
+def dummy_initial_data(migrator):
+    now = datetime.utcnow()
+    vumi_user = migrator.ndoh_control.get_or_create_user(migrator.ndoh_control.user, 'Vumi')
+    source_id = migrator.ndoh_control.create_source(name='Vumi Go', user_id=vumi_user['id'],
+                                                    created_at=now, updated_at=now)
+    # Create required ndoh-hub sources.
+    vumi_hub_user = migrator.ndoh_hub.get_or_create_user(migrator.ndoh_hub.user, 'Vumi')
+    migrator.ndoh_hub.create_source(name='CLINIC USSD App', authority='hw_full',
+                                    user_id=vumi_hub_user['id'], created_at=now, updated_at=now)
+    migrator.ndoh_hub.create_source(name='CHW USSD App', authority='hw_limited',
+                                    user_id=vumi_hub_user['id'], created_at=now, updated_at=now)
+    migrator.ndoh_hub.create_source(name='PUBLIC USSD App', authority='patient',
+                                    user_id=vumi_hub_user['id'], created_at=now, updated_at=now)
+    yield {
+        'control_source_id': source_id,
+    }
 
-# connection fixtures for all DBS
+
+@pytest.fixture(scope='module')
+def dummy_clinic_registration(migrator, dummy_initial_data):
+    now = datetime.utcnow()
+    migrator.ndoh_control.create_registration(
+        hcw_msisdn='+27825550000',
+        mom_msisdn='+27825550100',
+        mom_id_type='sa_id',
+        mom_lang='en',
+        mom_edd='2016-11-16',
+        mom_id_no='7001015550000',
+        mom_dob='1970-01-01',
+        clinic_code='357751',
+        authority='clinic',
+        source_id=dummy_initial_data['control_source_id'],
+        created_at=now,
+        updated_at=now,
+        mom_passport_origin=None,
+        consent=True
+    )
+
+
+def test_migrator(migrator, dummy_clinic_registration):
+    # Migrate registrations.
+    migrator.migrate_registrations()
+
+    # Check HCW Identities.
+    hcw_results = migrator.seed_identity.execute(
+        select([migrator.seed_identity.identity])
+        .where(
+            (migrator.seed_identity.identity.c.details[('addresses', 'msisdn', '+27825550000')] != null())
+            & (migrator.seed_identity.identity.c.details['role'].astext == 'hcw')
+        )
+    )
+    hwcs = hcw_results.fetchall()
+    assert len(hwcs) == 1
+
+    # Check mom Identities.
+
+    # Check Hub Registration.
